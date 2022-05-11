25535fc4a749ab3b623070f0a0311a64e4dfe3f5
DaniaZafar <99080115+DaniaZafar@users.noreply.github.com>
2022-05-11 12:37:20 +0500

    multiplecascading

---
+++ multiple_dropdown/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using multiple_dropdown.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace multiple_dropdown.Controllers
+{
+    public class HomeController : Controller
+    {
+        multipleDropdownEntities db = new multipleDropdownEntities();
+        public ActionResult Index()
+        {
+            List<Country> CountryList = db.Countries.ToList();
+            ViewBag.CountryList = new SelectList(CountryList, "CountryId", "CountryName");
+
+            return View();
+        }
+
+
+        public JsonResult GetStateList(int CountryId)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<State> StateList = db.States.Where(x => x.CountryId == CountryId).ToList();
+
+            return Json(StateList, JsonRequestBehavior.AllowGet);
+        }
+          
+        
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
